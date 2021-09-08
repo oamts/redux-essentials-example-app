23ac901bbed45eff9fbd58da8e4e3c9b4b916f07
Mateus <mateusoliveiraalmeida@gmail.com>
2021-09-07 22:05:38 -0300

    Add the AddPostForm

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 
 import { Navbar } from './app/Navbar'
 import { PostsList } from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             render={() => (
               <section>
                 <React.Fragment>
+                  <AddPostForm />
                   <PostsList />
                 </React.Fragment>
               </section>
